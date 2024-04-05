4f23f982c7a929ac2905ca8e0b1b464fa51ebaf8
Paweł Uchański <pawel.uchanski@euvic.pl>
2024-04-05 13:27:13 +0200

    change port to 8080

--- main.py
+++ main.py
@@ -400,4 +400,4 @@ async def read_index(request: Request):
 
 # Uvicorn start
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
